36d4df5d54b7292605a191c46740f040b956cd23
Crowdbotics <team@crowdbotics.com>
2020-12-21 22:16:26 +0000

    Initial commit.

---
+++ ios/flyzia_23509Tests/flyzia_23509Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface flyzia_23509 : XCTestCase
+
+@end
+
+@implementation flyzia_23509
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
